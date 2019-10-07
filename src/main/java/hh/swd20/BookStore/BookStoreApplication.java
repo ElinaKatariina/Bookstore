eb8b65695a9712a5871cf28052e32cce5c11054a
Elina Niemelä <elinaniemela@Elina-MacBook-Air.local>
2019-10-07 08:28:03 +0300

    Update 7.10.19

--- src/main/java/hh/swd20/BookStore/BookStoreApplication.java
+++ src/main/java/hh/swd20/BookStore/BookStoreApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.security.core.userdetails.User;
 import ch.qos.logback.classic.Logger;
 import hh.swd20.BookStore.domain.Book;
 import hh.swd20.BookStore.domain.BookRepository;
 import hh.swd20.BookStore.domain.Category;
 import hh.swd20.BookStore.domain.CategoryRepository;
+import hh.swd20.BookStore.domain.UserRepository;
 
 
 @SpringBootApplication
@@ -22,7 +23,7 @@ public class BookStoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner categoryDemo(BookRepository repository , CategoryRepository cRepository) {
+	public CommandLineRunner categoryDemo(BookRepository bRepository , CategoryRepository cRepository, UserRepository userRepo) {
 		return (args) -> {
 			log.info("save a couple of books");
 			cRepository.save(new Category("Drama"));
@@ -32,15 +33,22 @@ public class BookStoreApplication {
 			cRepository.save(new Category("Psykology"));
 			
 			
-				repository.save(new Book
+				bRepository.save(new Book
 						("Bridget Jones, Elämä jatkuu", "Helen Fielding", 1999, "978-951-1-27713-2", 6.50, cRepository.findByName("Drama").get(0)));
-				repository.save(new Book	
+				bRepository.save(new Book	
 						("Idiootit ympärilläni", "Thomas Erikson", 2017, "978-952-300-321-7", 17.95, cRepository.findByName("Psykology").get(0)));
-				repository.save(new Book
+				bRepository.save(new Book
 						("The Crown", "Robert Lacey", 2017, "978-951-1-32191-0", 26.95, cRepository.findByName("Biography").get(0)));
+			
+			//Create users
+			User user1 = new User("user", "user", "USER");
+			User user2 = new User("admin", "admin", "ADMIN");
+			userRepo.save(user1);
+			userRepo.save(user2);
+				
 				
 			log.info("fetch all books");
-			for (Book book : repository.findAll()) {
+			for (Book book : bRepository.findAll()) {
 				log.info(book.toString());
 			}
 		
--- src/main/java/hh/swd20/BookStore/WebSecurityConfig.java
+++ src/main/java/hh/swd20/BookStore/WebSecurityConfig.java
@@ -3,19 +3,28 @@ package hh.swd20.BookStore;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import hh.swd20.BookStore.controller.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDetailsServiceImpl userDetailService;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -35,7 +44,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.permitAll();
 	}
 	
-	@Bean
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailService).passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	
+	/*
+	 * @Bean
 	@Override
 	public UserDetailsService userDetailsService() {
 		List<UserDetails> users	= new ArrayList();
@@ -55,5 +71,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		
 		return new InMemoryUserDetailsManager(users);
 	}
-
+	 */
 }
